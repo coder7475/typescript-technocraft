654699a6bb1b0049bb611bc3d1a3b4604662c171
coder7475 <robiulhossain7475@gmail.com>
2024-04-30 21:43:31 +0600

    feat: learn typle

--- module1/src/nonPrimitive.ts
+++ module1/src/nonPrimitive.ts
@@ -1,8 +1,14 @@
 // Non Primitve Types
 // 1. Array
- // implicit
+// implicit
 let friends = ["saif", "niaj", 4];
 // explicit
 let collegues: string[] = ["saif"]; // only accept string 
 let nums: number[] = [5, 443]; // only accetps number
 let mix: (number | string)[] = ["niaj", 55]; // accepts both string and number
+
+/**
+ * * 2. Tuple ===> array ===> order ===> types of values
+ * * Limit count and type 
+ */
+let cord: [number, number] = [1, 4];
