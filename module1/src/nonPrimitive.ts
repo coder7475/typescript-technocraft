5016d97b5f3eff06b8c2cfa82e4793178cef638d
coder7475 <robiulhossain7475@gmail.com>
2024-04-30 21:40:41 +0600

    feat: Array Types in TS

---
+++ module1/src/nonPrimitive.ts
@@ -0,0 +1,8 @@
+// Non Primitve Types
+// 1. Array
+ // implicit
+let friends = ["saif", "niaj", 4];
+// explicit
+let collegues: string[] = ["saif"]; // only accept string 
+let nums: number[] = [5, 443]; // only accetps number
+let mix: (number | string)[] = ["niaj", 55]; // accepts both string and number
