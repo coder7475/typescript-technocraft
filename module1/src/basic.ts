f68fd4eef01778342d94c820ffa4eb2e03850134
coder7475 <robiulhossain7475@gmail.com>
2024-04-30 21:24:32 +0600

    feat: Implicit vs Explicit data type

---
+++ module1/src/basic.ts
@@ -0,0 +1,8 @@
+// basic data types 
+
+// string 
+// TS Infers own its own - implicit data type 
+let firstName = 'Robiul';
+// Explicit data type
+let lastName: string = "Hossain";
+// use Infer whenever possible e.g ts can correctly infer type
