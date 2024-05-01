d1fa6418786682cbc39cd29d6795222be535a825
coder7475 <robiulhossain7475@gmail.com>
2024-05-01 20:50:53 +0600

    feat: destructuring object

---
+++ module1/src/destructing.ts
@@ -0,0 +1,19 @@
+// Destructuring
+
+// Object destructuring
+{
+    const usr = {
+        id: 34,
+        name: {
+            firstName: "Hello",
+            middleName: "My",
+            lastName: "World"
+        },
+        constactNO: 445,
+        address: "nigeria"
+    }
+
+    const { constactNO, name: { middleName } } = usr;
+
+}
+
