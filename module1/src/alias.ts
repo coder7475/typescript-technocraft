7472280776aaba40781d116c81d5b1fa2f1899c3
coder7475 <robiulhossain7475@gmail.com>
2024-05-01 21:00:48 +0600

    feat: type alias for object

---
+++ module1/src/alias.ts
@@ -0,0 +1,30 @@
+{
+// --------------    
+// Type Alias
+type Student = {
+    name: string;
+    age: number;
+    gender: string;
+    contact?: number; // oprional
+}
+
+const std1: Student = {
+    name: "abdul",
+    age: 45,
+    gender: "testing"
+};
+
+const std2: Student = {
+    name: "Last",
+    age: 23,
+    gender: "Word",
+    contact: 4434
+}
+
+
+
+
+
+
+// -----------------
+}
