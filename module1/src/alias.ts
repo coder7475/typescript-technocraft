8727fe1434453f73e065eef7336ab4fe53d82faa
coder7475 <robiulhossain7475@gmail.com>
2024-05-01 21:03:53 +0600

    feat: type alias for function

--- module1/src/alias.ts
+++ module1/src/alias.ts
@@ -29,6 +29,10 @@ const userName: UserName = "LSD";
 const admin: IsAdmin = true;
 
 
+// type alias for function
+type Add = (num1: number, num2: number) => number;
+
+const add: Add = (num1, num2) => num1 + num2;
 
 // -----------------
 }
