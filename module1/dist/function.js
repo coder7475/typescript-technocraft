80d332270a25b19177c5ea68f51aec838dee0947
coder7475 <robiulhossain7475@gmail.com>
2024-05-01 19:32:52 +0600

    feat: recompile ts files

--- module1/dist/function.js
+++ module1/dist/function.js
@@ -4,7 +4,22 @@
  * * Normal Function vs Arrow Function
  */
 // Normal Function
-function add(num1, num2) {
+function add(num1, num2 = 10) {
     return num1 + num2;
 }
-add(true, 3);
+add(2, 3);
+// arrow function 
+// x, y = parameters type and return type is defined
+const addArrow = (x, y) => x + y;
+// function in object = method
+const User = {
+    name: "Robiul",
+    balance: 0,
+    // use normal annoymus function because of 'this' keyword
+    addBalance(balance) {
+        return this.balance + balance;
+    }
+};
+// Mapping
+const arr = [1, 2, 3, 4];
+const newArr = arr.map((elem) => elem * elem);
---
+++ module1/src/spreadRest.ts
@@ -0,0 +1,20 @@
+{
+    // Spread Operator
+    const bhais: string[] = ["A", "B", "C"];
+    const brothers: string[] = ["D", "E"];
+
+    // Argument of type 'string[]' is not assignable to parameter of type 'strin
+    // bhais.push(brothers)
+
+    const mentors = {
+        types: "M",
+        redxu: "N",
+        dbms: "X"
+    }
+
+    const teachers = {
+        prisma: "Firoz",
+        next: "TOn",
+        cloud: "kl"
+    }
+}
