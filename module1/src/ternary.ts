9e253ac3943cd05b51354a9d01f7b0d92ebb0b09
coder7475 <robiulhossain7475@gmail.com>
2024-05-01 21:33:45 +0600

    feat: learn nulllish coalishing operator

--- module1/src/ternary.ts
+++ module1/src/ternary.ts
@@ -1,5 +1,14 @@
 {
     // Ternary operator
     const age: number = 15;
-    age >= 5 ? console.log("adult") : console.log("Not");
+    age >= 18 ? console.log("adult") : console.log("Not Adult");
+
+    // Nullish coalescing Operator
+    // If decision based on Null / Undefined
+    // const isAuthen = ""; - { res: ''}
+    // const isAuthen = null; - { res: "Guest"}
+    const isAuthen = undefined;
+    const res = isAuthen ?? 'Guest';
+    const res2 = isAuthen ? isAuthen : "Guest"
+    console.log({ res }, { res2});
 }
