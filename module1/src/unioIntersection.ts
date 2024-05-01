f058a86d520cecc1115251aab601bc09fc6d8b3b
coder7475 <robiulhossain7475@gmail.com>
2024-05-01 21:09:27 +0600

    feat: union type for object

--- module1/src/unioIntersection.ts
+++ module1/src/unioIntersection.ts
@@ -2,4 +2,18 @@
     // Union Type - literat types union
     type FrontendDeveloper = 'fakibazDeveloper' | 'goodDeveloper';
     const newDev: FrontendDeveloper = "goodDeveloper";
+
+    type User = {
+        name: string;
+        email: string;
+        gender: 'male' | 'female';
+        blood: 'O' | 'A' | 'AB'
+    };
+
+    const ami: User = {
+        email: "roa",
+        name: "Fahad",
+        gender: "male",
+        blood: 'O'
+    }
 }
