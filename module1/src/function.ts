5c4bf7f3880aaf0ef9ed66dca1e27fec6cd3091c
coder7475 <robiulhossain7475@gmail.com>
2024-05-01 19:22:33 +0600

    feat: lean how to write method

--- module1/src/function.ts
+++ module1/src/function.ts
@@ -12,3 +12,14 @@ add(2, 3);
 // arrow function 
 // x, y = parameters type and return type is defined
 const addArrow = (x: number, y: number): number => x + y;
+
+// function in object = method
+const User = {
+    name: "Robiul",
+    balance: 0,
+    // use normal annoymus function because of 'this' keyword
+    addBalance(balance: number): number{
+        return this.balance + balance;
+    }
+}
+
