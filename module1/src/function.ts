d59ae10eb15aa2eaf1bd9b50f47cf62854f2a5ac
coder7475 <robiulhossain7475@gmail.com>
2024-05-01 19:14:23 +0600

    feat: learn arrow function

--- module1/src/function.ts
+++ module1/src/function.ts
@@ -3,8 +3,12 @@
  * * Normal Function vs Arrow Function
  */
 // Normal Function
-function add(num1: number, num2: number) {
+function add(num1: number, num2: number): number {
     return num1 + num2;
 }
 
 add(2, 3);
+
+// arrow function 
+// x, y = parameters type and return type is defined
+const addArrow = (x: number, y: number): number => x + y;
