0035d8e4cf319425c4e2936b419cdf767c33ac52
coder7475 <robiulhossain7475@gmail.com>
2024-05-03 19:18:12 +0600

    feat: learn interface

---
+++ module2/src/interface.ts
@@ -0,0 +1,13 @@
+// interface similiar to type 
+// usefull to create custom types for objects
+type User1 = {
+    name: string;
+    age: number;
+}
+
+type roll = number;
+
+interface User2 {
+    name: string;
+    age: number;
+}
