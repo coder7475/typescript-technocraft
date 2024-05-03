fc15b6b66fccba3b52103a3317815809a42488b5
coder7475 <robiulhossain7475@gmail.com>
2024-05-03 20:25:18 +0600

    feat: declare genrics for array of objects

--- module2/src/generics.ts
+++ module2/src/generics.ts
@@ -9,6 +9,18 @@ const rolls: GenericArr<number> = [3, 6, 8];
 const mentors: GenericArr<string> = ['a', 'b'];
 const boolArr: GenericArr<boolean> = [true, false];
 
+// generic array of objects
+const user: GenericArr<{ name: string, age: number}> = [
+    {
+        name: "Hello",
+        age: 35
+    },
+    {
+        name: "World",
+        age: 20
+    }
+]
+
 
 //
 }
