9b563d0e4bccb18c3ece2cd2eeb511f52ace2822
coder7475 <robiulhossain7475@gmail.com>
2024-05-03 19:36:25 +0600

    feat: intersection with interface

--- module2/src/interface.ts
+++ module2/src/interface.ts
@@ -1,24 +1,36 @@
-// interface similiar to type 
-// usefull to create custom types for objects
-type User1 = {
+{
+  // interface similiar to type
+  // usefull to create custom types for objects
+  type User1 = {
     name: string;
     age: number;
-}
+  };
 
-type roll = number;
-// intersection
-type UserWithRole = User1 & { role: string };
+  type roll = number;
+  // intersection
+  type UserWithRole = User1 & { role: string };
 
-// interface
-interface User2 {
+  // interface
+  interface User2 {
     name: string;
     age: number;
-}
+  }
 
-// using interface you can extend interface
-// interface
-interface UserWithRoleX extends User2 {
-    role: string
-}
+  // using interface you can extend interface
+  // interface
+  interface UserWithRoleX extends User2 {
+    role: string;
+  }
 
-// const user1:UserWithRole = 
+  const user1: UserWithRole = {
+    name: "A",
+    age: 4,
+    role: "done",
+  };
+
+  const user2: UserWithRoleX = {
+    name: "A",
+    age: 4,
+    role: "done",
+  };
+}
