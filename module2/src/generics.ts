edd0c1b801740d53e86253391afb1b45d96e6cda
coder7475 <robiulhossain7475@gmail.com>
2024-05-03 20:18:12 +0600

    feat: declare generics array type

---
+++ module2/src/generics.ts
@@ -0,0 +1,14 @@
+{
+//
+// Generics - Reusable generic type
+
+// declare a generic array
+type GenericArr<T> = Array<T>;
+
+const rolls: GenericArr<number> = [3, 6, 8];
+const mentors: GenericArr<string> = ['a', 'b'];
+const boolArr: GenericArr<boolean> = [true, false];
+
+
+//
+}
