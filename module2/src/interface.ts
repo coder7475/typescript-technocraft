d1c2e2170e408e4298acddfc2ce88e20f15044dc
coder7475 <robiulhossain7475@gmail.com>
2024-05-03 19:34:27 +0600

    feat: intersection with interface

--- module2/src/interface.ts
+++ module2/src/interface.ts
@@ -6,8 +6,19 @@ type User1 = {
 }
 
 type roll = number;
+// intersection
+type UserWithRole = User1 & { role: string };
 
+// interface
 interface User2 {
     name: string;
     age: number;
 }
+
+// using interface you can extend interface
+// interface
+interface UserWithRoleX extends User2 {
+    role: string
+}
+
+// const user1:UserWithRole = 
