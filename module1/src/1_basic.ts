9bd56482d55b70edb20a10f7f6121471736884bd
coder7475 <robiulhossain7475@gmail.com>
2025-05-01 19:34:10 +0600

    feat: learn primitive data types

--- module1/src/1_basic.ts
+++ module1/src/1_basic.ts
@@ -1,26 +1,38 @@
 // basic data types 
 
-// string 
-// TS Infers own its own - implicit data type 
+// ? string
+// TS Infers own its own - implicit data type
+// use Infer whenever possible e.g ts can correctly infer type
 let firstName = 'Robiul';
+
 // Explicit data type
 let lastName: string = "Hossain";
-// use Infer whenever possible e.g ts can correctly infer type
 
-// Primitive data types
+
+//? Primitive data types
 // 1. Number
 let num: number = 20;
+
 // 2. String
 let myName: string = "Fahad";
+
 // 3. Boolean
 let isAdmin: boolean = true;
+
 // 4. Null
 let nai: null = null;
+
 // 5. Undefined
 let asenai: undefined = undefined;
+
 // 6. Symbol
 let sym: symbol = Symbol();
 
-// Any Type - Off TS = JS
-let x;
+// Any Type - Off TS = JS - Not Recommanded
+let x: number;
 let y: any;
+
+x = 123
+x = 'ddd'
+y = 123;
+y = 'xxx'
