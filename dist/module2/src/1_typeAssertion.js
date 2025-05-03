40e7ba793ed182728a48c98d63b1c077e0371e2c
coder7475 <robiulhossain7475@gmail.com>
2025-05-03 19:29:11 +0600

    feat: learn type assertion

--- dist/module2/src/1_typeAssertion.js
+++ dist/module2/src/1_typeAssertion.js
@@ -2,11 +2,22 @@
 {
     // type assertion
     let anything;
+    console.log(anything);
     anything = "Apollo";
-    anything.length;
+    console.log(anything);
+    anything = 10;
+    console.log(anything.length);
+    //    (anything as string).length;
     const kgToGM = (value) => {
         // 
+        if (typeof value === "string") {
+            return parseFloat(value) * 1000;
+        }
+        else if (typeof value === "number") {
+            return value * 1000;
+        }
     };
+    console.log(kgToGM('10'));
     try {
     }
     catch (error) {
--- module2/src/1_typeAssertion.ts
+++ module2/src/1_typeAssertion.ts
@@ -2,20 +2,43 @@
     // type assertion
     let anything: any;
 
+    // console.log(anything);
+
     anything = "Apollo";
+
+    // console.log(anything);
+
+    anything = 10;
+    // console.log((anything as string).length);
     (anything as string).length;
 
-    const kgToGM = (value: string | number) => {
+    const kgToGM = (value: string | number): string | number | undefined => {
         // 
+        if (typeof value === "string") {
+            const convertedValue = parseFloat(value) * 1000;
+            return `The converted value is ${convertedValue}`;
+
+        } else if (typeof value === "number") {
+            return value * 1000;
+        }
     }
+
+    console.log(kgToGM('10'));
+
+    const result1 = kgToGM(10) as number;
+    const result2 = kgToGM('200') as string;
+
+    console.log(result1);
+    console.log(result2);
 
     // type assertion in error mesage
     type CustomError = {
         message: string
     }
+
     try {
 
     } catch (error) {
-        console.log((error as CustomError).message );
+        console.log((error as CustomError).message);
     }
 }
