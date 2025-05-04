61380951e5994ec205d0fd52985fe89395ba4a2d
coder7475 <robiulhossain7475@gmail.com>
2025-05-04 19:24:58 +0600

    feat: learning mapped types

---
+++ module2/src/9_mapped_types.ts
@@ -0,0 +1,12 @@
+{
+  //
+  // Mapped Types
+  // array of numbers
+  const arr: number[] = [1, 2, 3];
+
+  // map to a array of strings
+  const arr2: string[] = arr.map((num) => num.toString());
+
+  console.log(arr2);
+  //
+}
