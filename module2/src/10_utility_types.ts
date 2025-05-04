41e0a59f3cd49067dbd1d9bee752e0b21c78cdd1
coder7475 <robiulhossain7475@gmail.com>
2025-05-04 20:09:53 +0600

    feat: learn to omit types

--- module2/src/10_utility_types.ts
+++ module2/src/10_utility_types.ts
@@ -9,7 +9,11 @@
     phone: string;
   }
 
-  type NameAge = Pick<Person, 'name' | 'age'>;
+  type NameAge = Pick<Person, 'name' | 'age'>; // type has only name and age types
+
+  // Omit Types
+  type ContanctInfo = Omit<Person, 'name' | 'age'>; // type has only email and phone types
+
 
   //
 }
