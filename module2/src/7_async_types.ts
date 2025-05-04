f68f56dea217e6fb8c197de7b5b37fe79f5bf1d0
coder7475 <robiulhossain7475@gmail.com>
2025-05-04 19:11:50 +0600

    feat: create type for todo

--- module2/src/7_async_types.ts
+++ module2/src/7_async_types.ts
@@ -1,10 +1,19 @@
 {
+  // type definition for todos
+  type Todo = {
+    userId: number;
+    id: number;
+    title: string;
+    completed: boolean;
+  }
   // fetch todos from jsonplaceholders
-  const getTodos = async () => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+  const getTodos = async (): Promise<Todo> => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
     const data = await response.json();
     console.log(data);
+    return data;
   }
+  // calling the todo function
   getTodos();
 
   // promise
