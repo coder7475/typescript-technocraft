65b529055b0f0b2b3e017f9eb38c03c82cbb0088
coder7475 <robiulhossain7475@gmail.com>
2025-05-06 20:15:39 +0600

    feat: learn instanceof

--- module3/src/4_typeguard_instance.ts
+++ module3/src/4_typeguard_instance.ts
@@ -18,17 +18,52 @@
     }
 
   }
-
+  // Dog Class
   class Dog extends Animal {
     constructor(name: string, species: string) {
       super(name, species)
     }
 
     // methods
+    makeBark() {
+      console.log("Bark Bark")
+    }
   }
 
-  const dog1 = new Dog('white', "Dog");
+  // Cat Class
+  class Cat extends Animal {
+    constructor(name: string, species: string) {
+      super(name, species)
+    }
 
+    // methods
+    makeMeaw() {
+      console.log("Meaw!")
+    }
+  }
+
+  // type guard class/object
+
+  const getAnimal = (animal: Animal) => {
+    if (animal instanceof Dog) {
+      animal.makeBark()
+    } else if (animal instanceof Cat) {
+      animal.makeMeaw()
+    } else {
+      animal.makeSound()
+    }
+
+    return animal
+  }
+
+
+  const dog1 = new Dog('white', "Dog");
+  const cat = new Cat('tutu', "Cat");
+
+  console.log(getAnimal(cat))
+
+  const isDog = (animal: Animal): animal is Dog => animal instanceof Dog
+  console.log(isDog(dog1))
 
   //
 }
