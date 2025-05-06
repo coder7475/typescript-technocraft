17cfd95e599b6e1925be7e4e3f8b7e2be934c3e0
coder7475 <robiulhossain7475@gmail.com>
2025-05-06 21:52:03 +0600

    feat: learn protected properties

--- module3/src/5_access_modifier.ts
+++ module3/src/5_access_modifier.ts
@@ -4,7 +4,7 @@
   class BankAcc {
     constructor(
       readonly id: string,
-      public name: string,
+      protected name: string,
       private balance: number) {
       //
     }
@@ -18,9 +18,20 @@
     }
   }
 
+  // child class
+  class StudentAcc extends BankAcc {
+    getName() {
+      return this.name
+    }
+  }
+
   const acc1 = new BankAcc('111', 'rob', 42000)
   console.log(acc1)
+
   acc1.addDeposit(33)
   console.log(acc1)
+
+  const s1 = new StudentAcc('222', 'sifat', 43);
+  console.log(s1)
   //
 }
