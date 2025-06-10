ffa6861c1d4ae77c06a2b08eeb5e69a189ea91b3
coder7475 <robiulhossain7475@gmail.com>
2025-06-10 20:03:51 +0600

    feat: usecase 1 of generics

--- sl_workshop/5_generics.ts
+++ sl_workshop/5_generics.ts
@@ -9,5 +9,19 @@
   functionName<number>(123);
   functionName<string>("hello");
   functionName<boolean>(true);
+
+  functionName(123);
+  functionName("hello");
+  functionName(true);
+
+  // usecase 1
+  function mergeObjects<T, U>(obj1: T, obj2: U): T & U {
+    return { ...obj1, ...obj2 };
+  }
+
+  const merge = mergeObjects({ a: 1 }, { b: 2 });
+  console.log(merge);
+  ///
+
   ///
 }
